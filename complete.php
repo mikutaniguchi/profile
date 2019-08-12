f29a94c994a81f71fefe6288a2d2d3ce02783abf
miku.taniguchi <tani.emp417@gmail.com>
2019-08-12 05:54:42 +0000

    profile

---
+++ complete.php
@@ -0,0 +1,65 @@
+ <?php 
+        $name= $_POST['name'];
+        $mail = $_POST['mail'];
+        $content = $_POST['content'];
+        
+        // $company  = '';
+        // if(array_key_exists('company', $_POST)){
+        //     $company = $_POST['company'];
+        // }
+        
+        // $name  = '田中太郎';
+        // if(array_key_exists('name', $_POST)){
+        //     $name = $_POST['name'];
+        // }
+        
+        
+        // if($_POST){
+        //     $company = $_POST['company'];
+        //     $name = $_POST['name'];
+        // }
+  
+?>
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+        <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="intro.css">
+    <title>Miku Taniguchi</title>
+  </head>
+  <body>
+ 
+ 
+        <div class="container">
+            <h2 class="mt-4 pb-4 border-bottom">送信完了</h2>
+            <p>ありがとうございました。送信を受け付けました。</p>
+            
+            <h2>送信内容</h2>
+            <table class="table">                    
+                <tr><th>氏名</th><td><?php print htmlspecialchars($name, ENT_QUOTES, "UTF-8"); ?></td></tr>
+                <tr><th>メールアドレス</th><td><?php print htmlspecialchars($mail, ENT_QUOTES, "UTF-8"); ?></td></tr>
+                <tr><th>お問い合わせ内容</th><td><?php print nl2br(htmlspecialchars($content, ENT_QUOTES, "UTF-8")); ?></td></tr>
+            </table>
+            
+            <div class="text-center mb-4">
+                <a href="intro.php" class="btn btn-info btn-lg">戻る</a> 
+                            
+            </div>
+        </div>
+        <footer class="text-center pt-3 border-top">
+            &copy; 2019 MIKU Inc.
+        </footer>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS, then Font Awesome -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
+    </body>
+</html>
---
+++ intro.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+        <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="intro.css">
+    <title>Miku Taniguchi</title>
+  </head>
+  <body>
+    <header>
+      　
+      <nav class="navbar navbar-expand-md navbar-light">
+        <a href="#" class="navbar-brand">Profile</a>
+        <button type="button" class="navbar-toggler ml-auto" data-toggle="collapse" data-target="#nav-bar">
+                <span class="navbar-toggler-icon"></span>
+        </button>
+　　　　　　 <div class="collapse navbar-collapse row justify-content-end" id="nav-bar">
+               <ul class="navbar-nav mr-3">
+
+                   <li class="nav-item"><a href="#a" class="nav-link">About</a></li>
+                   <li class="nav-item "><a href="#b" class="nav-link">Career</a></li>
+                   <li class="nav-item"><a href="#c" class="nav-link">Reason</a></li>
+                   <li class="nav-item"><a href="#d" class="nav-link">Portfolio</a></li>
+                   <li class="nav-item"><a href="#f" class="nav-link">Contact</a></li>
+               </ul>
+           </div>
+       </nav>
+    <div class="bg-image pr-3">
+      <h1>Miku Taniguchi</h1>
+    </div>
+    </div>
+    </header>
+    <main>
+      <div id="a">
+      <h2 class="text-center mt-5 border-b pb-2">About</h2>
+      <div class="width-sm">
+      <table class="table table-bordered">
+    　<tr>
+     　　 <th class="text-center">生まれ</th><td>神奈川県平塚市（海と山が近い）</td>
+    　</tr>
+      <tr>
+    　　  <th class="text-center">生年月日</th><td>1990年4月17日</td>
+    　</tr>
+       
+      <tr>
+     　　 <th class="text-center">趣味</th><td>スノーボード...年間10回以上<br>
+        パン作り...メロンパン上手にできました<br>
+        旅行...最近は伊豆にハマり中</td>
+      </tr>
+        <tr>
+      <th class="text-center">学習中の言語</th><td>HTML、CSS、Bootstrap、jQuery、PHP/Laravel</td>
+      </tr>
+        <tr>
+      <th class="text-center">その他勉強中</th><td>Photoshop</td>
+      </tr>
+      <tr>
+      　<th class="text-center">モットー</th><td>失敗を恐れず何事にもチャレンジする！</td>
+      </tr>
+        </table>
+        </div>
+      </div>
+　<div id="b">
+  　　　<h2 class="text-center mt-5 border-b pb-2">Career</h2>
+  　　　<div class="width-sm">
+   <table class="table table-striped table-bordered">
+    <tr>
+　　  <th>学生時代</th><td>細かい作業が好きで、毛糸でバッグやマフラーを編んだりアクセサリー作りをする</td>
+　　</tr>
+    <tr>
+　　  <th>2012.4</th><td>大学で観光を学ぶゼミに入り、富士宮の観光ガイドブックや小田急線各停を回るガイドブックを製作</td>
+　　</tr>
+    <tr>
+  　　<th>2013.3</th><td>法政大学社会学部社会学科卒業</td>
+　　</tr>
+    <tr>
+　　  <th>2013.4</th><td>ゴルフ場経営の会社の事務職として働く<br>
+                     excel、wordがわからなかったため独学+excel資格取得</td>
+　　</tr>
+    <tr>
+  　　<th>2015.4</th><td>秘書兼経営企画部に異動<br>
+                      DM制作担当になり、初めは制作業者方とのやりとり<br>
+                      自身でも色彩の勉強、Illustratorを学び、DMを作成</td>
+　　</tr>
+    <tr>
+　　  <th>2016.7</th><td>能動的な仕事がしたいと思い、旅行webサイト・雑誌運営会社の広告営業職へ転職
+  　　　　　　　　　　　</td>
+　　</tr>
+    <tr>
+　　  <th>2017.7</th><td>サイトの見せ方、どうしたらPVが増えるかなどWebの仕組みに興味を持つも社風が合わず退職
+  <br>宮古島生活３ヶ月</td>
+  　</tr>
+    <tr>
+　　  <th>2017.12</th><td>スノーボードと英語環境に惹かれて野沢温泉生活</td>
+  　</tr>
+    <tr>
+  　　<th>2018.4</th><td>ワーキングホリデービザでニュージーランドへ<br>英語学校、バイト、スノーボード、山登りや旅行を満喫</td>
+  　</tr>
+    <tr>
+　　  <th>2018.1</th><td>日本の良さを再確認して1月に帰国<br>再び野沢温泉２ヶ月</td>
+  　</tr>
+    </table>
+    </div>
+ </div>
+
+    <div id="c">
+      <h2 class="text-center mt-5 border-b pb-2">Reason</h2>
+      <div class="width-sm">
+      <p class="text-center mt-3 mb-5"> ー 私がWebエンジニアになりたい理由 ー </p>
+      <h3 class="reason-h3">手に職をつけたい</h3>
+      <p class="p-5">旅をして様々な人に会う中で、本職を持っていて、自分の仕事に誇りを持っている人がたくさんいました。
+      仕事は仕事と割り切る訳ではなく、生活の一部として仕事があると捉えているように思いました。そんな生き方がかっこいいと思い、そうなりたいと思いました。</p>
+      <h3 class="reason-h3">自分が作ったもので人に喜んでもらいたい</h3>
+      <p class="p-5">小さい頃から細かい作業が好きで、何かを作って形になった時の瞬間がとても好きでした。それが人に喜んでもらえるものであればもっと嬉しくなりました。
+     何かを便利に使いやすくしたり、新しい交流を作ることができたり、どんなものでも作れる可能性があるwebエンジニアという仕事は私にとってとても夢があると思いました。</p>
+      <h3 class="reason-h3">理想の働き方</h3>
+      <p class="p-5">パソコン一つあれば作業ができる、スキルを身につけたらどこにいてもその仕事ができるというところに魅力を感じました。
+      旅行が好きであること、そして女性としてのキャリアを考えた時に在宅で仕事ができること、この２点が魅力を感じた理由です。
+      将来はリモートワークで働き、理想の働き方を実現したいです。</p>
+      </div>
+    </div>
+    
+    <div id="d">
+      <h2 class="text-center mt-5 mb-5 border-b pb-2">Portfolio</h2>
+       <div class="width-sm text-center">
+           <p class="mt-4">旅行の写真を共有するサービス</p>
+         <a href="https://tripper-miku.herokuapp.com/" target="_blank"><img src="portfolio.png" alt="portfolio" width="100%" height="auto" class="center">  
+         <p class="mt-4">https://tripper-miku.herokuapp.com/</p></a>
+            <p class="mt-5">コンセプト</p>
+            <p class="mt-4">旅行の感動を共有して、かつ他の人の投稿を参考にして次の旅行先を決められるようなものを
+            作りたいと思いました。旅行は行く季節などによっても見所が違うと思ったため、場所に加えて
+            月も検索できる機能をつけました。</p>
+           
+            <p class="mt-5">ログイン用</p>　 
+             <pclass="mt-4">メールアドレス：work.jp@gmail.com<br>
+             パスワード：041729</p>
+        </div>  
+        </div>    
+  
+          
+          
+          
+      <!--<h3 class="pr-h3 mt-5">好奇心旺盛</h3>-->
+      <!--<p class="mt-5">英語、バスケ、スノーボード、パン作り。興味を持ったものは挑戦して、今までできなかったことができるようになることがとても嬉しく感じる。-->
+      <!--</p>-->
+      <!--<h3 class="pr-h3 mt-5">行動力</h3>-->
+      <!--<p class="mt-5">やりたいと思ったものは固定概念にとらわれず、初心者でもその環境に飛び込む。バスケも英語もそうして徐々にレベルアップすることができた。</p>-->
+      
+    <div id="f">
+      <h2 class="text-center mt-5 border-b pb-2">Contact</h2>
+   <div class="width-sm">
+      <form action="complete.php"  method="POST">
+   <!-- 1行 -->
+   <div class="form-group row mt-4">
+       <label class="col-sm-3 col-form-label">名前</label>
+       <div class="col-sm-9">
+           <input type="text" class="form-control" name="name">
+       </div>
+   </div>
+
+   <!-- 1行 -->
+   <div class="form-group row mt-4">
+       <label class="col-sm-3 col-form-label">メールアドレス</label>
+       <div class="col-sm-9">
+           <input type="text" class="form-control" name="mail">
+       </div>
+   </div>
+
+   <div class="form-group row mt-4">
+       <label class="col-sm-3 col-form-label">お問い合わせ内容</label>
+       <div class="col-sm-9">
+           <input type="textarea" class="form-control" name="content" rows="4" cols="40">
+       </div>
+   </div>
+
+   <!-- 1行 -->
+   <div class="form-group row mt-3">
+       <div class="offset-sm-3 col-sm-9">
+           <button type="submit" class="btn btn-info">送信</button>
+       </div>
+   </div>
+  </div>
+  </form>
+  </div>
+ </main>
+        <footer class="text-center pt-3 border-top">
+            &copy; 2019 MIKU Inc.
+        </footer>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS, then Font Awesome -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
+    
+
+  </body>
+ </html>
